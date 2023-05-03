fac455603672ef1f28c7db946f82ba389597240f
Md Golam Moctader <moctader.golam@gmail.com>
2023-05-03 23:46:03 +0300

    DFS

---
+++ week 01 introduction to algorithomd and graph theory/Module 5:DFS/recursion_string.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+bool is_palindrom(string s)
+{
+    if (s=="" || s.size()==1)
+    {
+        return true;
+    }
+    int n=s.size();
+    string sub_string=s.substr(1, n-2);
+    return (is_palindrom(sub_string) && s[0]==s.back());
+}
+int main()
+{
+    string s;
+    cin>>s;
+    if (is_palindrom(s))
+    {
+        cout<<"yes";
+    }
+    else 
+        cout<<"No";
+    
+    return 0;
+}
