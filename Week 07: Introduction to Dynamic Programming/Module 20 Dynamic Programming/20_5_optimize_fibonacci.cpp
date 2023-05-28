3b8cbb550070a6605da3968fc9a063d812364d99
Md Golam Moctader <moctader.golam@gmail.com>
2023-05-28 13:48:09 +0300

    add

---
+++ Week 07: Introduction to Dynamic Programming/Module 20 Dynamic Programming/20_5_optimize_fibonacci.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+ll ara[101];
+
+ll fib(int x)
+{
+    if (x<=2)
+    {
+        return 1;
+    }
+    if (ara[x] !=0)
+    {
+        return ara[x];
+    }
+    ara[x]=fib(x-1)+fib(x-2);
+    return ara[x];
+    
+    
+}
+
+int main()
+{
+    cout<<fib(2)<<endl;
+
+    return 0;
+}
